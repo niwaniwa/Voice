d321fe754c60732e7bfbdb8f07e32922ced33c4d
niwaniwa <konya.kokekokko@gmail.com>
2020-05-05 16:56:49 +0900

    エフェクトインターフェースの追加   - IEffectModuleの追加

---
+++ VoiceSystem/Effect/Abs/IEffectModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voice.VoiceSystem.Effect.Abs
+{
+	public interface IEffectModule
+	{
+
+		bool CanExecute();
+
+		void Execute();
+
+	}
+}
