7e7a4c2b485dae93736f7b22ac779ffe695fbeaf
niwaniwa <konya.kokekokko@gmail.com>
2020-05-05 16:55:38 +0900

    エフェクト格納用のリストの追加とプログラム整形   - エフェクト格納用のリスト追加   - プログラムの整列

--- VoiceSystem/VoiceExtractor.cs
+++ VoiceSystem/VoiceExtractor.cs
@@ -1,11 +1,22 @@
 ﻿using NAudio.CoreAudioApi;
+using NAudio.Dsp;
 using NAudio.Wave;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Voice.MVVM;
+using Voice.Core.Event;
 using Voice.VoiceSystem;
+using Voice.VoiceSystem.Effect;
+using Voice.VoiceSystem.Effect.Abs;
+using NAudio.Wave.SampleProviders;
+using System.Threading;
 
 namespace Voice.VoVoiceSystemice
 {
@@ -15,7 +26,16 @@ namespace Voice.VoVoiceSystemice
 
 		private WaveInEvent waveIn = new WaveInEvent();
 		private WaveFileWriter waveWriter;
+		private BufferedWaveProvider bufferedWaveProvider;
+		private AudioService audioService;
+		public List<IEffectModule> effectModules = new List<IEffectModule>();
+
 		private const string filePath = @"";
+
+		public VoiceExtractor()
+		{
+			audioService = AudioService.Instance;
+		}
 
 		public void Start()
 		{
@@ -24,46 +44,75 @@ namespace Voice.VoVoiceSystemice
 				return;
 			}
 
-			// 入力
-			var bufferedWaveProvider = new BufferedWaveProvider(new WaveFormat(44100, 16, 2));
-			
+			var wavFormat = new WaveFormat(44100, 16, 2);
 
+			bufferedWaveProvider = new BufferedWaveProvider(wavFormat);
 			// 音量
 			var wavProvider = new VolumeWaveProvider16(bufferedWaveProvider);
 			wavProvider.Volume = 0.1f;
 
-			var device = new MMDeviceEnumerator().GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+			// デバッグ用Wav出力設定
+			waveWriter = new WaveFileWriter(filePath, bufferedWaveProvider.WaveFormat);
 
-			IsRecording = true;
-			//外部からの音声入力を受け付け開始
-			waveWriter = new WaveFileWriter(filePath, waveIn.WaveFormat);
-			using (var wavPlayer = new WasapiOut(device, AudioClientShareMode.Shared, false, 200))
+			// 外部からの音声入力を受け付け設定
+			waveIn.WaveFormat = wavFormat;
+			waveIn.DeviceNumber = audioService.CaputureDevices.ToList<MMDevice>().IndexOf(audioService.SelectedCaptureDevice);
+			waveIn.DataAvailable += DataAvailable;
+			waveIn.RecordingStopped += ( _, __) =>
 			{
-				waveIn.DataAvailable += (_, e) =>
-				{
-					bufferedWaveProvider.AddSamples(e.Buffer, 0, e.BytesRecorded);
-					waveWriter.Write(e.Buffer, 0, e.BytesRecorded);
-					waveWriter.Flush();
-				};
-				waveIn.StartRecording();
+				waveIn.StopRecording();
+				waveIn.Dispose();
+				waveWriter.Dispose();
+				IsRecording = false;
+			};
 
-			}
+			// start
+			waveIn.StartRecording();
+			IsRecording = true;
+
+			var wavPlayer = new WasapiOut();
+			wavPlayer.Init(wavProvider);
+			wavPlayer.Play();
+
+		}
+
+		private void DataAvailable(object sender, WaveInEventArgs wave)
+		{
+			// buffer渡し
+			bufferedWaveProvider.AddSamples(wave.Buffer, 0, wave.BytesRecorded);
+
+			// wav write
+			waveWriter.Write(wave.Buffer, 0, wave.BytesRecorded);
+			waveWriter.Flush();
 
 		}
 
 		public void Stop()
 		{
-			waveIn.RecordingStopped += (_, __) =>
-			{
-				waveWriter.Flush();
-			};
 			waveIn.StopRecording();
-			IsRecording = false;
 		}
+		
+		public void AddEffector(IEffectModule module)
+		{
+			EffectModules.Add(module);
+		}
+
 
 		public bool IsRecording
 		{
 			get; private set;
+		}
+
+		public List<IEffectModule> EffectModules
+		{
+			get
+			{
+				return effectModules;
+			}
+			private set
+			{
+				effectModules = value;
+			}
 		}
 
 	}
